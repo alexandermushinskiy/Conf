83afd2ded3346fa1074d0768d31694db70ca938e
SU\alexander.mushinsky <alexander.mushinskiy@sigma.software>
2017-12-16 19:10:37 +0200

    Add missing files

---
+++ Conf.Management.WebApi/Models/Requests/CreateRequestModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Conf.Management.WebApi.Models.Requests
+{
+    public class CreateRequestModel
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime FinishDate { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string OwnerName { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string OwnerEmail { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Venue { get; set; }
+    }
+}
