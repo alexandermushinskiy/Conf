9f5e2bf7d0b484cabc50d0e32116a71735e6220d
SU\alexander.mushinsky <alexander.mushinskiy@sigma.software>
2017-12-16 18:54:39 +0200

    Update DataAccess project Add obj/bin to ignore

---
+++ Conf.Management.DataAccess/Repositories/ConferenceRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Conf.Management.Entities;
+using Conf.Management.Extensibility.Repositories;
+using Conf.Management.Infrastructure.DataAccess.Dummy;
+
+namespace Conf.Management.Infrastructure.DataAccess.Repositories
+{
+    internal class ConferenceRepository : IConferenceRepository
+    {
+        private static readonly List<Conference> ConferenceStore = DummyStorage.GetData().ToList();
+
+        public ConferenceRepository()
+        {
+        }
+
+        public IEnumerable<Conference> GetAll()
+        {
+            return ConferenceStore;
+        }
+        
+        public Conference GetById(Guid id)
+        {
+            return ConferenceStore.Find(conf => conf.Id == id);
+        }
+
+        public void Create(Conference conference)
+        {
+            ConferenceStore.Add(conference);
+        }
+
+        public void Update(Conference conference)
+        {
+            int index = ConferenceStore.FindIndex(conf => conf.Id == conference.Id);
+            ConferenceStore[index] = conference;
+        }
+
+        public void Delete(Conference conference)
+        {
+            ConferenceStore.Remove(conference);
+        }
+    }
+}
--- Conf.Management.WebApi/Startup.cs
+++ Conf.Management.WebApi/Startup.cs
@@ -1,20 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-using Autofac;
-
+﻿using Autofac;
 using AutoMapper;
-
 using Conf.Management.DataAccess;
-
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-
 using Newtonsoft.Json.Serialization;
 
 namespace Conf.Management.WebApi
